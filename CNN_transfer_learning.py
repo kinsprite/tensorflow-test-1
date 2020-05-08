ee3d6445384f755bb2adf356d4aa8e0c0abd7288
kinsprite <qxqs11@126.com>
2020-05-08 18:59:01 +0800

    update format

--- CNN_transfer_learning.py
+++ CNN_transfer_learning.py
@@ -1,5 +1,4 @@
-import os
-import numpy as np
+# import numpy as np
 import matplotlib.pyplot as plt
 
 import tensorflow as tf
@@ -8,13 +7,13 @@ import tensorflow as tf
 from tensorflow.compat.v1 import ConfigProto
 from tensorflow.compat.v1 import InteractiveSession
 
+# Load Data
+import tensorflow_datasets as tfds
+# tfds.disable_progress_bar()
+
 config = ConfigProto()
 config.gpu_options.allow_growth = True
 session = InteractiveSession(config=config)
-
-# Load Data
-import tensorflow_datasets as tfds
-#tfds.disable_progress_bar()
 
 (raw_train, raw_validation, raw_test), metadata = tfds.load(
     'cats_vs_dogs',
@@ -30,19 +29,21 @@ print(raw_test)
 get_label_name = metadata.features['label'].int2str
 
 for image, label in raw_train.take(2):
-  plt.figure()
-  plt.imshow(image)
-  plt.title(get_label_name(label))
+    plt.figure()
+    plt.imshow(image)
+    plt.title(get_label_name(label))
 plt.show()
 
 # Format the Data
-IMG_SIZE = 160 # All images will be resized to 160x160
+IMG_SIZE = 160  # All images will be resized to 160x160
+
 
 def format_example(image, label):
-  image = tf.cast(image, tf.float32)
-  image = (image/127.5) - 1
-  image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
-  return image, label
+    image = tf.cast(image, tf.float32)
+    image = (image / 127.5) - 1
+    image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+    return image, label
+
 
 train = raw_train.map(format_example)
 validation = raw_validation.map(format_example)
@@ -57,7 +58,7 @@ test_batches = test.batch(BATCH_SIZE)
 
 # Inspect a batch of data
 for image_batch, label_batch in train_batches.take(1):
-   pass
+    pass
 
 image_batch.shape
 
@@ -76,7 +77,6 @@ base_model.trainable = False
 # Let's take a look at the base model architecture
 base_model.summary()
 
-
 # Add a classification head
 global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
 feature_batch_average = global_average_layer(feature_batch)
@@ -86,12 +86,8 @@ prediction_layer = tf.keras.layers.Dense(1)
 prediction_batch = prediction_layer(feature_batch_average)
 print(prediction_batch.shape)
 
-model = tf.keras.Sequential([
-  base_model,
-  global_average_layer,
-  prediction_layer
-])
-
+model = tf.keras.Sequential(
+    [base_model, global_average_layer, prediction_layer])
 
 # Compile the model
 base_learning_rate = 0.0001
@@ -103,14 +99,13 @@ model.summary()
 
 # Train the model
 initial_epochs = 10
-validation_steps=20
+validation_steps = 20
 
-loss0, accuracy0 = model.evaluate(validation_batches, steps = validation_steps)
+loss0, accuracy0 = model.evaluate(validation_batches, steps=validation_steps)
 
 history = model.fit(train_batches,
                     epochs=initial_epochs,
                     validation_data=validation_batches)
-
 
 # Learning curves
 acc = history.history['accuracy']
@@ -125,7 +120,7 @@ plt.plot(acc, label='Training Accuracy')
 plt.plot(val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
 plt.ylabel('Accuracy')
-plt.ylim([min(plt.ylim()),1])
+plt.ylim([min(plt.ylim()), 1])
 plt.title('Training and Validation Accuracy')
 
 plt.subplot(2, 1, 2)
@@ -133,7 +128,7 @@ plt.plot(loss, label='Training Loss')
 plt.plot(val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.ylabel('Cross Entropy')
-plt.ylim([0,1.0])
+plt.ylim([0, 1.0])
 plt.title('Training and Validation Loss')
 plt.xlabel('epoch')
 plt.show()
--- mnist_example_adv.py
+++ mnist_example_adv.py
@@ -19,18 +19,19 @@ test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
 
 
 class MyModel(Model):
-  def __init__(self):
-    super(MyModel, self).__init__()
-    self.conv1 = Conv2D(32, 3, activation='relu')
-    self.flatten = Flatten()
-    self.d1 = Dense(128, activation='relu')
-    self.d2 = Dense(10, activation='softmax')
+    def __init__(self):
+        super(MyModel, self).__init__()
+        self.conv1 = Conv2D(32, 3, activation='relu')
+        self.flatten = Flatten()
+        self.d1 = Dense(128, activation='relu')
+        self.d2 = Dense(10, activation='softmax')
 
-  def call(self, x):
-    x = self.conv1(x)
-    x = self.flatten(x)
-    x = self.d1(x)
-    return self.d2(x)
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
+
 
 model = MyModel()
 
@@ -44,44 +45,46 @@ train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='train_accuracy
 test_loss = tf.keras.metrics.Mean(name='test_loss')
 test_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(name='test_accuracy')
 
+
 @tf.function
 def train_step(images, labels):
-  with tf.GradientTape() as tape:
-    predictions = model(images)
-    loss = loss_object(labels, predictions)
-  gradients = tape.gradient(loss, model.trainable_variables)
-  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    with tf.GradientTape() as tape:
+        predictions = model(images)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-  train_loss(loss)
-  train_accuracy(labels, predictions)
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
 
 @tf.function
 def test_step(images, labels):
-  predictions = model(images)
-  t_loss = loss_object(labels, predictions)
+    predictions = model(images)
+    t_loss = loss_object(labels, predictions)
 
-  test_loss(t_loss)
-  test_accuracy(labels, predictions)
+    test_loss(t_loss)
+    test_accuracy(labels, predictions)
 
 
 EPOCHS = 5
 
 for epoch in range(EPOCHS):
-  # 在下一个epoch开始时，重置评估指标
-  train_loss.reset_states()
-  train_accuracy.reset_states()
-  test_loss.reset_states()
-  test_accuracy.reset_states()
+    # 在下一个epoch开始时，重置评估指标
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    test_loss.reset_states()
+    test_accuracy.reset_states()
 
-  for images, labels in train_ds:
-    train_step(images, labels)
+    for images, labels in train_ds:
+        train_step(images, labels)
 
-  for test_images, test_labels in test_ds:
-    test_step(test_images, test_labels)
+    for test_images, test_labels in test_ds:
+        test_step(test_images, test_labels)
 
-  template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
-  print (template.format(epoch+1,
-                         train_loss.result(),
-                         train_accuracy.result()*100,
-                         test_loss.result(),
-                         test_accuracy.result()*100))
+    template = 'Epoch {}, Loss: {}, Accuracy: {}, Test Loss: {}, Test Accuracy: {}'
+    print(template.format(epoch+1,
+                          train_loss.result(),
+                          train_accuracy.result()*100,
+                          test_loss.result(),
+                          test_accuracy.result()*100))
